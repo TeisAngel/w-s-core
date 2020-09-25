d40184fbf93cc27588061736d24b74d419cf68b4
Nazarii <besedovskiy7@gmail.com>
2020-09-26 00:42:55 +0300

    Update spec for position

--- __tests__/position.spec.js
+++ __tests__/position.spec.js
@@ -1,15 +1,47 @@
 const { Position } = require('../index');
 
 describe('Game position', () => {
-  test('it creates position', () => {
+  it('creates position', () => {
     expect(Position.createPosition(0, 0)).toBeDefined();
   });
 
-  test('it creates position with right coordinates', () => {
-    const x = 1, y = 1;
+  it('creates position with right coordinates', () => {
+    const x = 1,
+      y = 1;
     const pos = Position.createPosition(x, y);
 
     expect(Position.getX(pos)).toBe(x);
     expect(Position.getY(pos)).toBe(y);
+  });
+
+  describe('#add', () => {
+    it('returns function', () => {
+      const pos = Position.createPosition(0, 0);
+      expect(typeof Position.add(pos)).toBe('function');
+    });
+
+    it('adds two position', () => {
+      const pos1 = Position.createPosition(34345, 21);
+      const pos2 = Position.createPosition(0, -121);
+
+      const res = Position.add(pos1)(pos2);
+
+      expect(Position.getX(res)).toBe(
+        Position.getX(pos1) + Position.getX(pos2),
+      );
+      expect(Position.getY(res)).toBe(
+        Position.getY(pos1) + Position.getY(pos2),
+      );
+    });
+  });
+
+  describe('#asArray', () => {
+    it('returns position as array', () => {
+      const pos = Position.createPosition(-12, 923);
+      expect(Position.asArray(pos)).toStrictEqual([
+        Position.getX(pos),
+        Position.getY(pos),
+      ]);
+    });
   });
 });
