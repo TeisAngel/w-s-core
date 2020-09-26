aaf0bea3a3da208eaee92e7013fdc2f199b6eb3b
Nazarii <besedovskiy7@gmail.com>
2020-09-26 23:06:45 +0300

    Update action spec

--- __tests__/action.spec.js
+++ __tests__/action.spec.js
@@ -1,7 +1,7 @@
-const { Game, Action } = require("../index");
-const _ = require("lodash/fp");
+const { Game, Action } = require('../index');
+const _ = require('lodash/fp');
 
-describe("Game moves", () => {
+describe('Game moves', () => {
   const prepare = () => {
     const game = Game.createGame({
       playersCount: 2,
@@ -14,38 +14,38 @@ describe("Game moves", () => {
     return { game, player, logic };
   };
 
-  describe("Action logic creation", () => {
-    test("returns action logic", () => {
+  describe('Action logic creation', () => {
+    it('returns action logic', () => {
       expect(Action.createActionsLogic()).toBeDefined();
     });
   });
 
-  describe("Action creation", () => {
-    test("retuns action producer", () => {
-      const { logic, player } = prepare();
+  describe('Action creation', () => {
+    it('retuns action producer', () => {
+      const { player } = prepare();
 
-      expect(Action.producer(logic)(player)).toBeDefined();
+      expect(Action.producer(player)).toBeDefined();
     });
 
-    test("retuns action", () => {
-      const { logic, player } = prepare();
+    it('retuns action', () => {
+      const { player } = prepare();
 
-      const create = Action.producer(logic)(player);
+      const create = Action.producer(player);
 
       expect(create({})).toBeDefined();
     });
   });
 
-  describe("Actions consuming", () => {
-    test("returns action consumer", () => {
+  describe('Actions consuming', () => {
+    it('returns action consumer', () => {
       const { logic, player } = prepare();
       expect(Action.consumer(logic)(player)).toBeDefined();
     });
 
-    test("retuns new game state", () => {
+    it('retuns new game state', () => {
       const { logic, game, player } = prepare();
 
-      const create = Action.producer(logic)(player);
+      const create = Action.producer(player);
 
       const apply = Action.consumer(logic)(game);
 
@@ -55,40 +55,39 @@ describe("Game moves", () => {
     });
   });
 
-  describe("Consumer logic", () => {
-    const prepareWithNewLogic = () => {
-      const logic = Action.createActionsLogic({
-        consumer: ({ game, move }) => {
-          if (move.isCorrect == true) return game;
+  describe('Consumer logic', () => {
+    const prepareWithNewLogic = () => ({
+      ...prepare(),
+      logic: Action.createActionsLogic({
+        consumer: ({ game, action }) => {
+          if (action.payload.isCorrect == true) return game;
           return Game.makeInvalid(game);
         },
-      });
+      }),
+    });
 
-      return { ...prepare(), logic };
-    };
-
-    test("returns valid game state if move is valid", () => {
+    it('returns valid game state if move is valid', () => {
       const { logic, game, player } = prepareWithNewLogic();
 
-      const create = Action.producer(logic)(player);
+      const create = Action.producer(player);
 
       const apply = Action.consumer(logic)(game);
 
       const nextState = apply(create({ isCorrect: true }));
 
-      expect(game).not.toBe(nextState);
+      expect(Game.isValid(nextState)).toBe(true);
     });
 
-    test("returns invalid game state if action is invalid", () => {
+    it('returns invalid game state if action is invalid', () => {
       const { logic, game, player } = prepareWithNewLogic();
 
-      const create = Action.producer(logic)(player);
+      const create = Action.producer(player);
 
       const apply = Action.consumer(logic)(game);
 
       const nextState = apply(create({ isCorrect: false }));
 
-      expect(Game.isValid(nextState)).not.toBe(false);
+      expect(Game.isValid(nextState)).toBe(false);
     });
   });
 });
