d2ecf757f0f4cc377873a6b00812c49c09dd5fe9
Nazarii <besedovskiy7@gmail.com>
2020-09-27 13:25:48 +0300

    Add map to game

--- __tests__/game.spec.js
+++ __tests__/game.spec.js
@@ -1,4 +1,5 @@
-const { createGame, isValid, makeInvalid } = require('../lib/game');
+const { createGame, isValid, makeInvalid, getMap, setMap } = require('../lib/game');
+const { createMap } = require('../lib/map');
 
 describe('Game', () => {
   it('creates new game', () => {
@@ -19,6 +20,26 @@ describe('Game', () => {
     it('returns true for a invalid game', () => {
       const invalidGame = makeInvalid(createGame());
       expect(isValid(invalidGame)).toBe(false);
+    });
+  });
+
+  describe('#getMap', () => {
+    it('returns map', () => {
+      expect(getMap(createGame())).toBeDefined();
+    })
+  });
+
+  describe('#setMap', () => {
+    it('returns valid game state', () => {
+      const game = createGame();
+      const map = createMap();
+      expect(isValid(setMap(game)(map))).toBe(true);
+    });
+    it('returns new game instance', () => {
+      const game = createGame();
+      const map = createMap();
+      const nextGameState = setMap(game)(map);
+      expect(game).not.toBe(nextGameState);
     });
   });
 });
